910e38b9be50e03fbd1b7b6c81d44064d7a53880
Mohammad Shaker <mohammad.shaker.103@gmail.com>
2020-07-05 04:14:46 +0200

    Initial Commit

---
+++ ADecadeOfMovies/DetailViewController.swift
@@ -0,0 +1,40 @@
+//
+//  DetailViewController.swift
+//  ADecadeOfMovies
+//
+//  Created by Mohammad Shaker on 7/5/20.
+//  Copyright © 2020 Mohammad Shaker. All rights reserved.
+//
+
+import UIKit
+
+class DetailViewController: UIViewController {
+
+    @IBOutlet weak var detailDescriptionLabel: UILabel!
+
+
+    func configureView() {
+        // Update the user interface for the detail item.
+        if let detail = detailItem {
+            if let label = detailDescriptionLabel {
+                label.text = detail.description
+            }
+        }
+    }
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+        configureView()
+    }
+
+    var detailItem: NSDate? {
+        didSet {
+            // Update the view.
+            configureView()
+        }
+    }
+
+
+}
+
